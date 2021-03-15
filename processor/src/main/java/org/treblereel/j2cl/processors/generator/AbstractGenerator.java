73291ea772cfb851a80f45669e056803b6fea386
Dmitrii Tikhomirov <chani.liet@gmail.com>
2021-03-15 18:33:46 -0400

    added license headers and formating

--- processor/src/main/java/org/treblereel/j2cl/processors/generator/AbstractGenerator.java
+++ processor/src/main/java/org/treblereel/j2cl/processors/generator/AbstractGenerator.java
@@ -1,40 +1,59 @@
+/*
+ * Copyright © 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.treblereel.j2cl.processors.generator;
 
-import org.treblereel.j2cl.processors.annotations.GWT3EntryPoint;
-import org.treblereel.j2cl.processors.context.AptContext;
-import org.treblereel.j2cl.processors.exception.GenerationException;
-
-import javax.lang.model.element.Element;
-import javax.tools.Diagnostic;
-import javax.tools.FileObject;
-import javax.tools.StandardLocation;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
+import javax.lang.model.element.Element;
+import javax.tools.Diagnostic;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+import org.treblereel.j2cl.processors.context.AptContext;
+import org.treblereel.j2cl.processors.exception.GenerationException;
 
 public abstract class AbstractGenerator {
 
-    protected final AptContext context;
+  protected final AptContext context;
 
-    public AbstractGenerator(AptContext context, Class<? extends Annotation> annotation) {
-        this.context = context;
-        context.register(annotation, this);
+  public AbstractGenerator(AptContext context, Class<? extends Annotation> annotation) {
+    this.context = context;
+    context.register(annotation, this);
+  }
+
+  public abstract void generate(Element element);
+
+  protected void writeResource(String filename, String path, String content) {
+    try {
+      FileObject file =
+          context
+              .getProcessingEnv()
+              .getFiler()
+              .createResource(StandardLocation.SOURCE_OUTPUT, path, filename);
+      PrintWriter pw = new PrintWriter(new OutputStreamWriter(file.openOutputStream(), "UTF-8"));
+      pw.print(content);
+      pw.close();
+    } catch (IOException e) {
+      context
+          .getProcessingEnv()
+          .getMessager()
+          .printMessage(Diagnostic.Kind.ERROR, "Failed to write file: " + e);
+      throw new GenerationException("Failed to write file: " + e, e);
     }
-
-    public abstract void generate(Element element);
-
-    protected void writeResource(String filename, String path, String content) {
-        try {
-            FileObject file =
-                    context.getProcessingEnv().getFiler().createResource(StandardLocation.SOURCE_OUTPUT, path, filename);
-            PrintWriter pw = new PrintWriter(new OutputStreamWriter(file.openOutputStream(), "UTF-8"));
-            pw.print(content);
-            pw.close();
-        } catch (IOException e) {
-            context.getProcessingEnv().getMessager().printMessage(Diagnostic.Kind.ERROR, "Failed to write file: " + e);
-            throw new GenerationException("Failed to write file: " + e, e);
-        }
-    }
-
+  }
 }
--- processor/src/main/java/org/treblereel/j2cl/processors/generator/GWT3ExportGenerator.java
+++ processor/src/main/java/org/treblereel/j2cl/processors/generator/GWT3ExportGenerator.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright © 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.treblereel.j2cl.processors.generator;
 
 import com.google.auto.common.MoreElements;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.lang.model.element.*;
 import jsinterop.annotations.JsMethod;
-import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 import org.treblereel.j2cl.processors.annotations.ES6Module;
@@ -11,163 +29,185 @@ import org.treblereel.j2cl.processors.annotations.GWT3Export;
 import org.treblereel.j2cl.processors.context.AptContext;
 import org.treblereel.j2cl.processors.exception.GenerationException;
 
-import javax.lang.model.element.*;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class GWT3ExportGenerator extends AbstractGenerator {
 
-    public GWT3ExportGenerator(AptContext context) {
-        super(context, GWT3Export.class);
-    }
+  public GWT3ExportGenerator(AptContext context) {
+    super(context, GWT3Export.class);
+  }
 
-    @Override
-    public void generate(Element element) {
-        TypeElement parent = (TypeElement) element;
+  @Override
+  public void generate(Element element) {
+    TypeElement parent = (TypeElement) element;
 
-        check(parent);
+    check(parent);
 
-        Set<Element> elements = parent.getEnclosedElements()
-                .stream()
-                .filter(elm -> elm.getKind().isField() || elm.getKind().equals(ElementKind.METHOD))
-                .filter(elm -> elm.getAnnotation(JsMethod.class) != null || elm.getAnnotation(JsProperty.class) != null)
-                .filter(elm -> elm.getModifiers().contains(Modifier.PUBLIC))
-                .collect(Collectors.toSet());
+    Set<Element> elements =
+        parent.getEnclosedElements().stream()
+            .filter(elm -> elm.getKind().isField() || elm.getKind().equals(ElementKind.METHOD))
+            .filter(
+                elm ->
+                    elm.getAnnotation(JsMethod.class) != null
+                        || elm.getAnnotation(JsProperty.class) != null)
+            .filter(elm -> elm.getModifiers().contains(Modifier.PUBLIC))
+            .collect(Collectors.toSet());
 
-        elements.forEach(this::check);
+    elements.forEach(this::check);
 
-        generate(parent, elements);
-    }
+    generate(parent, elements);
+  }
 
-    private void generate(TypeElement parent, Set<Element> elements) {
-        StringBuffer source = new StringBuffer();
-        generateClassExport(parent, source);
-        generateStaticFieldsOrMethods(parent, elements, source);
+  private void generate(TypeElement parent, Set<Element> elements) {
+    StringBuffer source = new StringBuffer();
+    generateClassExport(parent, source);
+    generateStaticFieldsOrMethods(parent, elements, source);
 
-        String className = parent.getSimpleName().toString();
-        String classPkg = MoreElements.getPackage(parent).getQualifiedName().toString();
+    String className = parent.getSimpleName().toString();
+    String classPkg = MoreElements.getPackage(parent).getQualifiedName().toString();
 
-        writeResource(className + ".native.js", classPkg, source.toString());
-    }
+    writeResource(className + ".native.js", classPkg, source.toString());
+  }
 
-    private void generateStaticFieldsOrMethods(TypeElement parent, Set<Element> elements, StringBuffer source) {
-        elements.stream().filter(elm -> elm.getModifiers().contains(Modifier.STATIC)).forEach(element -> {
-            if (element.getKind().isField()) {
+  private void generateStaticFieldsOrMethods(
+      TypeElement parent, Set<Element> elements, StringBuffer source) {
+    elements.stream()
+        .filter(elm -> elm.getModifiers().contains(Modifier.STATIC))
+        .forEach(
+            element -> {
+              if (element.getKind().isField()) {
                 generateStaticField((VariableElement) element, parent, source);
-            } else {
+              } else {
                 generateStaticMethod((ExecutableElement) element, parent, source);
-            }
-        });
+              }
+            });
+  }
+
+  private void generateStaticField(
+      VariableElement element, TypeElement parent, StringBuffer source) {
+    source.append("goog.exportProperty(_");
+    source.append(parent.getSimpleName());
+    source.append(", '");
+    source.append(element.getSimpleName());
+    source.append("', ");
+    source.append(parent.getSimpleName());
+    source.append(".$static_");
+    source.append(element.getSimpleName());
+    source.append("__");
+    source.append(parent.getQualifiedName().toString().replaceAll("\\.", "_"));
+    source.append(");");
+    source.append(System.lineSeparator());
+  }
+
+  private void generateStaticMethod(
+      ExecutableElement element, TypeElement parent, StringBuffer source) {
+    source.append("goog.exportSymbol('");
+    maybeAddNamespace(parent.getAnnotation(GWT3Export.class), source);
+    source.append(getTypeName(parent));
+    source.append(".");
+    source.append(element.getSimpleName().toString());
+    source.append("', ");
+    source.append(parent.getSimpleName());
+    source.append(".");
+    source.append(element.getSimpleName().toString());
+    source.append(");");
+    source.append(System.lineSeparator());
+  }
+
+  private void generateClassExport(TypeElement parent, StringBuffer source) {
+    source.append("const _");
+    source.append(parent.getSimpleName());
+    source.append(" = ");
+
+    if (parent.getAnnotation(JsType.class) == null) {
+      source.append(parent.getSimpleName());
+      source.append(".$create__;");
+    } else {
+      source.append(parent.getQualifiedName().toString().replaceAll("\\.", "_"));
     }
 
-    private void generateStaticField(VariableElement element, TypeElement parent, StringBuffer source) {
-        source.append("goog.exportProperty(_");
-        source.append(parent.getSimpleName());
-        source.append(", '");
-        source.append(element.getSimpleName());
-        source.append("', ");
-        source.append(parent.getSimpleName());
-        source.append(".$static_");
-        source.append(element.getSimpleName());
-        source.append("__");
-        source.append(parent.getQualifiedName().toString().replaceAll("\\.", "_"));
-        source.append(");");
-        source.append(System.lineSeparator());
+    source.append(System.lineSeparator());
+
+    source.append("goog.exportSymbol('");
+    maybeAddNamespace(parent.getAnnotation(GWT3Export.class), source);
+    source.append(getTypeName(parent));
+    source.append("', _");
+    source.append(parent.getSimpleName());
+    source.append(");");
+    source.append(System.lineSeparator());
+  }
+
+  private void check(Element element) {
+    if (element.getKind().isField()) {
+      check((VariableElement) element);
+    } else {
+      check((ExecutableElement) element);
     }
+  }
 
-    private void generateStaticMethod(ExecutableElement element, TypeElement parent, StringBuffer source) {
-        source.append("goog.exportSymbol('");
-        maybeAddNamespace(parent.getAnnotation(GWT3Export.class), source);
-        source.append(getTypeName(parent));
-        source.append(".");
-        source.append(element.getSimpleName().toString());
-        source.append("', ");
-        source.append(parent.getSimpleName());
-        source.append(".");
-        source.append(element.getSimpleName().toString());
-        source.append(");");
-        source.append(System.lineSeparator());
+  private void check(TypeElement parent) {
+    if (!parent.getModifiers().contains(Modifier.PUBLIC)) {
+      throw new GenerationException(
+          "Class,  that contains methods/fields annotated with "
+              + GWT3Export.class.getCanonicalName()
+              + ", must be public");
     }
-
-    private void generateClassExport(TypeElement parent, StringBuffer source) {
-        source.append("const _");
-        source.append(parent.getSimpleName());
-        source.append(" = ");
-
-        if (parent.getAnnotation(JsType.class) == null) {
-            source.append(parent.getSimpleName());
-            source.append(".$create__;");
-        } else {
-            source.append(parent.getQualifiedName().toString().replaceAll("\\.", "_"));
-        }
-
-        source.append(System.lineSeparator());
-
-        source.append("goog.exportSymbol('");
-        maybeAddNamespace(parent.getAnnotation(GWT3Export.class), source);
-        source.append(getTypeName(parent));
-        source.append("', _");
-        source.append(parent.getSimpleName());
-        source.append(");");
-        source.append(System.lineSeparator());
+    if (parent.getModifiers().contains(Modifier.ABSTRACT)) {
+      throw new GenerationException(
+          "Class,  that contains methods/fields annotated with "
+              + GWT3Export.class.getCanonicalName()
+              + ", mustn't be abstract");
     }
-
-    private void check(Element element) {
-        if (element.getKind().isField()) {
-            check((VariableElement) element);
-        } else {
-            check((ExecutableElement) element);
-        }
-
+    if (parent.getAnnotation(JsType.class) != null
+        && parent.getAnnotation(JsType.class).isNative()) {
+      throw new GenerationException(
+          "If Class,  that contains methods/fields annotated with "
+              + GWT3Export.class.getCanonicalName()
+              + ", is @JsType, it mustn't be isNative=true");
     }
-
-    private void check(TypeElement parent) {
-        if (!parent.getModifiers().contains(Modifier.PUBLIC)) {
-            throw new GenerationException("Class,  that contains methods/fields annotated with " + GWT3Export.class.getCanonicalName() + ", must be public");
-        }
-        if (parent.getModifiers().contains(Modifier.ABSTRACT)) {
-            throw new GenerationException("Class,  that contains methods/fields annotated with " + GWT3Export.class.getCanonicalName() + ", mustn't be abstract");
-        }
-        if (parent.getAnnotation(JsType.class) != null && parent.getAnnotation(JsType.class).isNative()) {
-            throw new GenerationException("If Class,  that contains methods/fields annotated with " + GWT3Export.class.getCanonicalName() + ", is @JsType, it mustn't be isNative=true");
-        }
-        if (parent.getAnnotation(GWT3EntryPoint.class) != null) {
-            throw new GenerationException("If Class,  that contains methods/fields annotated with " + GWT3Export.class.getCanonicalName() + ", mustn't be annotated with @GWT3EntryPoint");
-        }
-        if (parent.getAnnotation(ES6Module.class) != null) {
-            throw new GenerationException("If Class,  that contains methods/fields annotated with " + GWT3Export.class.getCanonicalName() + ", mustn't be annotated with @ES6Module");
-        }
-
+    if (parent.getAnnotation(GWT3EntryPoint.class) != null) {
+      throw new GenerationException(
+          "If Class,  that contains methods/fields annotated with "
+              + GWT3Export.class.getCanonicalName()
+              + ", mustn't be annotated with @GWT3EntryPoint");
     }
-
-    private void check(VariableElement field) {
-        if (!field.getModifiers().contains(Modifier.PUBLIC)) {
-            throw new GenerationException("Field,  annotated with " + GWT3Export.class.getCanonicalName() + ", must be public");
-        }
+    if (parent.getAnnotation(ES6Module.class) != null) {
+      throw new GenerationException(
+          "If Class,  that contains methods/fields annotated with "
+              + GWT3Export.class.getCanonicalName()
+              + ", mustn't be annotated with @ES6Module");
     }
+  }
 
-    private void check(ExecutableElement method) {
-        if (!method.getModifiers().contains(Modifier.PUBLIC)) {
-            throw new GenerationException("Method,  annotated with " + GWT3Export.class.getCanonicalName() + ", must be public");
-        }
-        if (method.getModifiers().contains(Modifier.NATIVE)) {
-            throw new GenerationException("Method,  annotated with " + GWT3Export.class.getCanonicalName() + ", mustn't be native");
-        }
+  private void check(VariableElement field) {
+    if (!field.getModifiers().contains(Modifier.PUBLIC)) {
+      throw new GenerationException(
+          "Field,  annotated with " + GWT3Export.class.getCanonicalName() + ", must be public");
     }
+  }
 
-    private void maybeAddNamespace(GWT3Export gwt3Export, StringBuffer stringBuffer) {
-        if (gwt3Export != null && !gwt3Export.namespace().equals("<auto>") && !gwt3Export.name().isEmpty()) {
-            stringBuffer.append(gwt3Export.namespace()).append(".");
-        }
+  private void check(ExecutableElement method) {
+    if (!method.getModifiers().contains(Modifier.PUBLIC)) {
+      throw new GenerationException(
+          "Method,  annotated with " + GWT3Export.class.getCanonicalName() + ", must be public");
     }
-
-
-    private String getTypeName(TypeElement parent) {
-        GWT3Export gwt3Export = parent.getAnnotation(GWT3Export.class);
-        if (gwt3Export != null && !gwt3Export.name().equals("<auto>") && !gwt3Export.name().isEmpty()) {
-            return parent.getAnnotation(GWT3Export.class).name();
-        }
-        return parent.getQualifiedName().toString();
+    if (method.getModifiers().contains(Modifier.NATIVE)) {
+      throw new GenerationException(
+          "Method,  annotated with " + GWT3Export.class.getCanonicalName() + ", mustn't be native");
     }
+  }
 
+  private void maybeAddNamespace(GWT3Export gwt3Export, StringBuffer stringBuffer) {
+    if (gwt3Export != null
+        && !gwt3Export.namespace().equals("<auto>")
+        && !gwt3Export.name().isEmpty()) {
+      stringBuffer.append(gwt3Export.namespace()).append(".");
+    }
+  }
+
+  private String getTypeName(TypeElement parent) {
+    GWT3Export gwt3Export = parent.getAnnotation(GWT3Export.class);
+    if (gwt3Export != null && !gwt3Export.name().equals("<auto>") && !gwt3Export.name().isEmpty()) {
+      return parent.getAnnotation(GWT3Export.class).name();
+    }
+    return parent.getQualifiedName().toString();
+  }
 }
