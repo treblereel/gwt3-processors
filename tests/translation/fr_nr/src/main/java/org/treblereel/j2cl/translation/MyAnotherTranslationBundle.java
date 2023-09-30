47312542059f772a9266397d592a5f41c129c25e
Dmitrii Tikhomirov <chani.liet@gmail.com>
2023-09-30 14:06:19 -0700

    tests re-org

---
+++ tests/translation/fr_nr/src/main/java/org/treblereel/j2cl/translation/MyAnotherTranslationBundle.java
@@ -0,0 +1,47 @@
+package org.treblereel.j2cl.translation;
+
+import org.treblereel.j2cl.processors.annotations.TranslationBundle;
+import org.treblereel.j2cl.processors.annotations.TranslationKey;
+
+@TranslationBundle
+public interface MyAnotherTranslationBundle {
+
+  @TranslationKey(defaultValue = "Good bye {$arg} !")
+  String bye(String arg);
+
+  @TranslationKey(defaultValue = "{$arg} and {$arg}")
+  String dups(String arg);
+
+  @TranslationKey(defaultValue = "{$br}", html = true)
+  String br(String br);
+
+  @TranslationKey(defaultValue = "{$arg}", html = true, unescapeHtmlEntities = true)
+  String divContent(String arg);
+
+  @TranslationKey(defaultValue = "{$arg}", unescapeHtmlEntities = true)
+  String divContent2(String arg);
+
+  @TranslationKey(
+      defaultValue =
+          "<div id=\"LC1813\" class=\"blob-code blob-code-inner js-file-line\"><span class=\"pl-c\">{$text}</span></div>")
+  String htmlContent(String text);
+
+  @TranslationKey(
+      defaultValue =
+          "<div id=\"LC1813\" class=\"blob-code blob-code-inner js-file-line\"><span class=\"pl-c\">{$text}</span></div>",
+      html = true)
+  String htmlContentHtmlTrue(String text);
+
+  @TranslationKey(
+      defaultValue =
+          "<div id=\"LC1813\" class=\"blob-code blob-code-inner js-file-line\"><span class=\"pl-c\">{$text}</span></div>",
+      unescapeHtmlEntities = true)
+  String htmlContentUnescapeHtmlEntitiesTrue(String text);
+
+  @TranslationKey(
+      defaultValue =
+          "<div id=\"LC1813\" class=\"blob-code blob-code-inner js-file-line\"><span class=\"pl-c\">{$text}</span></div>",
+      html = true,
+      unescapeHtmlEntities = true)
+  String htmlContentHtmlTrueUnescapeHtmlEntitiesTrue(String text);
+}
---
+++ tests/translation/no_bundle/src/main/java/org/treblereel/j2cl/translation/App.java
@@ -0,0 +1,11 @@
+package org.treblereel.j2cl.translation;
+
+import org.treblereel.j2cl.processors.annotations.GWT3EntryPoint;
+
+public class App {
+
+  static MyTranslationBundle bundle = new org.treblereel.j2cl.translation.MyTranslationBundleImpl();
+
+  @GWT3EntryPoint
+  public void init() {}
+}
